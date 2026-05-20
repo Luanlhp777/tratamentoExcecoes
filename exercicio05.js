03c79929dd960e41f9a6075af96f03ca3af094f8
Luanlhp777 <pereira.luan@gmail.com>
2026-05-20 19:36:27 -0300

    Adiciona aula de Algoritmos

--- exercicio05.js
+++ exercicio05.js
@@ -1,2 +1,27 @@
 const prompt = require('prompt-sync')()
 
+let saldo = 1000;
+
+const valor = prompt("Digite o valor do saque: ");
+
+try {
+    if (isNaN(valor)) {
+        throw new Error("Valor inválido.");
+    }
+
+    if (Number(valor) <= 0) {
+        throw new Error("O valor deve ser positivo.");
+    }
+
+    if (Number(valor) > saldo) {
+        throw new Error("Saldo insuficiente.");
+    }
+
+    saldo -= Number(valor);
+
+    console.log("Saque realizado com sucesso.");
+    console.log("Saldo atual: ", saldo);
+
+} catch (erro) {
+    console.log("Erro: ", erro.message);
+}
